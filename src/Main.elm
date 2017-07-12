6b0f764ccb68c78c8eacf6cfcd804107560f429f
Antonio Villagra De La Cruz <antonio.villagra.de.la.cruz@gmail.com>
2017-07-12 02:30:19 +0200

    Move View to its own file

--- src/Main.elm
+++ src/Main.elm
@@ -1,18 +1,11 @@
-import Html exposing (..)
-import List
 import Navigation
-import UrlParser exposing((</>))
-
-import Page.Country
-import Page.Dish
-import Page.Home
-
 
 import Model exposing (Model, initialModel)
 import Msg exposing (Msg (..))
 import Update exposing (update, initialCmd)
+import View exposing (view)
 
-import Route exposing (Route (..), route, initialRoute)
+import Route exposing (initialRoute)
 
 -- MAIN --
 main : Program Never Model Msg
@@ -30,56 +23,6 @@ init location =
   ( initialModel (initialRoute location)
   , initialCmd (initialRoute location)
   )
-
--- VIEW --
-view : Model -> Html Msg
-view model =
-  div [] [ page model ]
-
-page : Model -> Html Msg
-page model =
-  case model.currentLocation of
-    Just route ->
-      case route of
-        Route.Home ->
-          Page.Home.view model
-        Route.Country id ->
-          let
-            country = 
-              List.head 
-                (List.filter 
-                  (\c -> c.id == id) 
-                  model.countries
-                )
-
-            dishes = model.countryDishes
-          in
-            case country of
-              Just country ->
-                Page.Country.view country dishes
-              Nothing ->
-                div [] [ text "404 - Not Found" ]
-        Route.Dish id ->
-          let
-            country =
-              List.head 
-                (List.filter 
-                  (\c -> c.id == String.left 2 id)
-                  model.countries
-                )
-            dish = model.currentDish
-          in
-            case country of
-              Just country ->
-                case dish of
-                  Just dish ->
-                    Page.Dish.view country dish
-                  Nothing ->
-                    div [] [ text "404 - Not Found" ]
-              Nothing ->
-                div [] [ text "404 - Not Found" ]
-    Nothing ->
-      Page.Home.view model
 
 -- SUBSCRIPTIONS --
 subscriptions : Model -> Sub Msg
---
+++ src/View.elm
@@ -0,0 +1,63 @@
+module View exposing (view)
+
+import Html exposing (..)
+
+import Model exposing (Model)
+import Msg exposing (Msg)
+
+import Page.Country
+import Page.Dish
+import Page.Home
+
+import Route exposing (Route (..))
+
+-- VIEW --
+view : Model -> Html Msg
+view model =
+  div [] [ page model ]
+
+-- PAGE --
+page : Model -> Html Msg
+page model =
+  case model.currentLocation of
+    Just route ->
+      case route of
+        Route.Home ->
+          Page.Home.view model
+        Route.Country id ->
+          let
+            country = 
+              List.head 
+                (List.filter 
+                  (\c -> c.id == id) 
+                  model.countries
+                )
+
+            dishes = model.countryDishes
+          in
+            case country of
+              Just country ->
+                Page.Country.view country dishes
+              Nothing ->
+                div [] [ text "404 - Not Found" ]
+        Route.Dish id ->
+          let
+            country =
+              List.head 
+                (List.filter 
+                  (\c -> c.id == String.left 2 id)
+                  model.countries
+                )
+            dish = model.currentDish
+          in
+            case country of
+              Just country ->
+                case dish of
+                  Just dish ->
+                    Page.Dish.view country dish
+                  Nothing ->
+                    div [] [ text "404 - Not Found" ]
+              Nothing ->
+                div [] [ text "404 - Not Found" ]
+    Nothing ->
+      Page.Home.view model
